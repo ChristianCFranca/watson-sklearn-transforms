5cb8addb00aa41751c44789f7a1b9cc914f7afd7
ChristianCFranca <62810702+ChristianCFranca@users.noreply.github.com>
2021-11-17 02:31:20 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,20 +1,38 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import Pipeline
 
-
-# All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
-
+class CategorizeColumns(BaseEstimator, TransformerMixin):
+    def __init__(self): # Respectivamente
+        return
+        
+    def try_float(self, number):
+        try:
+            number = float(number)
+            return True
+        except ValueError:
+            return False
+    
+    def correct_float(self, number):
+        try:
+            number = float(number)
+            return number
+        except ValueError:
+            return 0
+    
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
-        # Primeiro realizamos a cópia do DataFrame 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
-
+        
+        data['CHECKING_BALANCE_CATEGORICAL'] = (X['CHECKING_BALANCE'].apply(self.try_float) == False).apply(lambda x: 1 if x else 0)
+        data['CHECKING_BALANCE'] = X['CHECKING_BALANCE'].apply(self.correct_float).fillna(0.0)
+        
+        data['EXISTING_SAVINGS_CATEGORICAL'] = (X['EXISTING_SAVINGS'].apply(self.try_float) == False).apply(lambda x: 1 if x else 0)
+        data['EXISTING_SAVINGS'] = X['EXISTING_SAVINGS'].apply(self.correct_float).fillna(208.53)
+        
+        return data
+    
 class EncodeColumns(BaseEstimator, TransformerMixin):
     def __init__(self, encoders, columns): # Respectivamente
         self.enc_col_dict = {col: encoders[i] for i, col in enumerate(columns)}
